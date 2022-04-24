651e0921d68ebb37d6a994f363af1f23e6708890
raadhosieni <raadhosieni@yahoo.com>
2022-04-24 23:37:09 +0300

    adjust input to be webcam video

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
 let objectDetector;
 let video;
 
-function setup() {
-  createCanvas(600, 400);
-  video = createCapture(VIDEO);
-  video.hide();
-  objectDetector = ml5.objectDetector("cocossd");
+function preload() {
+  objectDetector = ml5.objectDetector("cocossd", {}, modelLoaded);
+  video = loadImage("cat-dog.jpeg");
 }
 
-function draw() {
-  image(video, 0, 0, width, height);
+function modelLoaded() {
+  console.log("model loaded");
+
   objectDetector.detect(video, gotDetections);
 }
 
@@ -18,12 +17,23 @@ function gotDetections(err, results) {
     console.log(err);
   }
 
-  for (let i = 0; i < results.length; i++) {
-    const object = results[i];
-    noFill();
+  results.forEach((element) => {
     stroke(0, 255, 0);
-    strokeWeight(3);
-    rect(object.x, object.y, boject.width, object.height);
-    alert(object.label);
-  }
+    strokeWeight(4);
+    noFill();
+    rect(element.x, element.y, element.width, element.height);
+    noStroke();
+    fill(255);
+    textSize(18);
+    text(element.label, element.x + element.width / 2, element.y + 20);
+  });
+}
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  image(video, 0, 0);
+  objectDetector.detect(video, gotDetections);
 }
